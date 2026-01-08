e3a81b29c804b7a92f1771e4176548e8846f631d
Dmitriy_Syworov <dmitriysyworov1986.com@gmail.com>
2026-01-08 19:32:41 +0300

    New_dire

---
+++ GO_Concurency/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	chRand := make(chan int)
+	chRes := make(chan int)
+	go generateRandom(chRand)
+	for i := 0; i < 10; i++{
+	rand := <-chRand
+	go powerRand(rand, chRes)
+		res := <-chRes
+		fmt.Print(res, " ")
+	}
+	close(chRand)
+	close(chRes)
+}
+func generateRandom(chn chan int) {
+	for i := 0; i < 10; i++ {
+		chn <- rand.Intn(100)
+	}
+}
+func powerRand(rand int, ch chan int) {
+	ch <- rand * rand
+}
